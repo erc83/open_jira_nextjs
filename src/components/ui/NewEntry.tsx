763b4a0e0dce2a0ab9e54149bd272fb537913b8d
erc83 <espinoza.eric@gmail.com>
2023-05-16 22:24:32 -0400

    funciones NewEntry.tsx

--- src/components/ui/NewEntry.tsx
+++ src/components/ui/NewEntry.tsx
@@ -1,48 +1,86 @@
+import { ChangeEvent, useState } from "react";
+
 import { Box, Button, TextField } from "@mui/material"
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';   /* importamos el Icono */
 import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined'; /* El nombre de lo puedo cambiar este es por defecto */
 
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = useState(false)  // estoy agregando
+
+    const [inputValue, setInputValue] = useState('')
+    const [touched, setTouched] = useState( true )
+
+    const onTextFieldChanges = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {   // event.  tradicional en javascript podemos ver el evento  
+        setInputValue( event.target.value)
+    }
+
+    const onSave = () => {
+        if( inputValue.length === 0) return;
+
+        console.log({ inputValue }) // estructurando el objeto
+    }
+
+
   return (
     <Box sx={{ marginBottom:2 , paddingX: 2 }}>  
 
-        {/* button para desplegar el TextField */}
+        {
+            isAdding ? (   // aqui puedo tranformar en un componente porque no se va a expandir mas
+                <>
+                    <TextField 
+                        fullWidth
+                        sx={{ marginTop:2, marginBottom: 1 }}
+                        placeholder="Nueva entrada"
+                        autoFocus
+                        multiline
+                        label='Nueva entrada'
+                        helperText={ inputValue.length <= 0 && touched && 'Ingrese un Valor' }
+                        error= { inputValue.length <= 0 && touched }
+                        value={ inputValue }
+                        onChange={ onTextFieldChanges }
+                        onBlur={ () => setTouched( true ) }
+                    />
+                    <Box display='flex' justifyContent='space-between'>
+                        <Button
+                            variant='text'
+                            onClick={() => setIsAdding( false )}
+                            >
+                            Cancelar
+                        </Button>
+                        <Button
+                            variant='outlined'        
+                            color='secondary'
+                            endIcon={ <SaveOutlinedIcon /> }   /* Usamos unicono */
+                            onClick={ onSave }
+                            >
+                            Guardar
+                        </Button>
+                    </Box>
+                </>
+            ) : (
+                <>
+                    <Button
+                        startIcon={<AddCircleOutlinedIcon /> }
+                        fullWidth
+                        variant="outlined"
+                        onClick={ ()  => setIsAdding( true )}
+                    >
+                        Agregar Tarea
+                    </Button>
+                </>
+            )
+        }
+       
 
-        <Button
-            startIcon={<AddCircleOutlinedIcon /> }
-            fullWidth
-            variant="outlined"
 
-        >
-            Agregar Tarea
-        </Button>
+        
 
 
-        <TextField 
-            fullWidth
-            sx={{ marginTop:2, marginBottom: 1 }}
-            placeholder="Nueva entrada"
-            autoFocus
-            multiline
-            label='Nueva entrada'
-            helperText='Ingrese un valor'
-        />
+        
 
 
-        <Box display='flex' justifyContent='space-between'>
-            <Button
-                variant='text'
-                >
-                Cancelar
-            </Button>
-            <Button
-                variant='outlined'        
-                color='secondary'
-                endIcon={ <SaveOutlinedIcon /> }   /* Usamos unicono */
-                >
-                Guardar
-            </Button>
-        </Box>
+
     </Box>
   )
 }
